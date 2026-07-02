e3ba7f3854f1fe94400725f614c211d7d99de612
Jimee Kang <pbcjimee@gmail.com>
2026-07-03 08:06:56 +1000

    chore: remove invoice work artifacts

--- tmp/invoice-work/help-copy.mjs
+++
@@ -1,4 +0,0 @@
-﻿import { FileBlob, SpreadsheetFile } from "@oai/artifact-tool";
-const workbookPath = String.raw`C:\Users\kjm12\OneDrive\바탕 화면\Paint Buddy & Co\Progress Invoice\Timbaworx\inv2911_Timbaworx-9 Clarence Street Balgowlah_quote3210\inv2911_Timbaworx-9 Clarence Street Balgowlah_quote3210.xlsx`;
-const workbook = await SpreadsheetFile.importXlsx(await FileBlob.load(workbookPath));
-console.log(workbook.help("worksheet copy", { include: "index,examples,notes", maxChars: 8000 }).ndjson);
